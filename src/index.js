f1e72f7f31d81d9648d9ad4838adfca170c85a18
Luis <lfmnovaes@gmail.com>
2021-10-12 19:40:43 -0300

    Generate cards
    * Generate small cards from a list of array list
    * Saving content for future use

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import './style.css';
 import { getMovieById } from './movie.js';
 import { getSerieById } from './series.js';
+import { createCard } from './card.js';
 
 const main = document.getElementsByTagName('main');
 const content = `
@@ -19,11 +20,36 @@ const loadMovie = async () => {
 loadMovie();
 */
 
-const loadSerie = async () => {
-  let serie;
-  await getSerieById().then((s) => { serie = s; });
-  console.log(serie);
-  console.log(serie.name);
+const showIDs = [
+  'tt0903747', // Breaking Bad
+  'tt0944947' // Game of Thrones
+];
+
+const div = document.createElement('div');
+div.className = 'row row-cols-1 row-cols-md-3 g-4';
+const loadShow = async () => {
+  let show;
+  await getSerieById().then((s) => { show = s; });
+  //console.log(show);
+  //div.innerHTML += createCard(show);
 };
 
-loadSerie();
+const showList = [];
+
+const loadShows = async () => {
+  for (const show of showIDs) {
+    const content = await getSerieById(show);
+    showList.push(content);
+    //console.log(content);
+    div.innerHTML += createCard(content);
+  }
+};
+
+loadShow();
+loadShows();
+
+console.log(showList);
+setTimeout(() => console.log(showList[0]), 1000);
+
+main[0].append(div);
+//main[0].insertAdjacentHTML('beforeend', createCard());
