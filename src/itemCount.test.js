915160e1acc57b90a1322b1747bc9bd98225678b
Luis <lfmnovaes@gmail.com>
2021-10-15 17:32:41 -0300

    One more test for item count

--- src/itemCount.test.js
+++ src/itemCount.test.js
@@ -11,7 +11,10 @@ describe('Counter shows test', () => {
   test('Not supported genre', () => {
     expect(navClick(nav, 'Action', arr3, arr6, arr8)).toBe(0);
   });
-  test('Adding new task to the DOM', () => {
+  test('Supported genre with 8 IDs', () => {
     expect(navClick(nav, 'Drama', arr8, arrEmpty, arr3)).toBe(8);
+  });
+  test('Supported genre with 3 IDs', () => {
+    expect(navClick(nav, 'Documentary', arr8, arrEmpty, arr3)).toBe(3);
   });
 });
